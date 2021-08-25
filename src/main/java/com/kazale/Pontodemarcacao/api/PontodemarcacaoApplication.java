d2ee7ac11f19b2710f0db13fb670507eb25739a3
Fabio Costa <fscosta114@gmail.com>
2021-08-25 16:51:27 -0300

    incializando o projeto de marcaçaõ de ponto

---
+++ src/main/java/com/kazale/Pontodemarcacao/api/PontodemarcacaoApplication.java
@@ -0,0 +1,13 @@
+package com.kazale.Pontodemarcacao.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontodemarcacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontodemarcacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kazale/Pontodemarcacao/api/PontodemarcacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazale.Pontodemarcacao.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontodemarcacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
